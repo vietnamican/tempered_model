8cd397b4682654231ec984584858cadd73f1a3fe
vietnamican <vietnamican@gmail>
2021-02-13 20:41:16 +0700

    change structure of Resnet50Truncate

--- index_resnet50.py
+++ index_resnet50.py
@@ -9,6 +9,11 @@ try:
 except ImportError:
     from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
+try:
+    import torch_xla.core.xla_model as xm
+except:
+    pass
+
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
@@ -28,20 +33,48 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=36)
+device = 'cpu'
 
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=128, shuffle=False, num_workers=36)
+if device == 'tpu':
+    trainset = torchvision.datasets.CIFAR10(
+    root='./data', train=True, download=True, transform=transform_train)
+
+    train_sampler = torch.utils.data.distributed.DistributedSampler(
+                trainset,
+                num_replicas=xm.xrt_world_size(),
+                rank=xm.get_ordinal(),
+                shuffle=True
+            )
+
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=128, num_workers=4, sampler=train_sampler)
+
+    testset = torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform_test)
+
+    test_sampler = torch.utils.data.distributed.DistributedSampler(
+                testset,
+                num_replicas=xm.xrt_world_size(),
+                rank=xm.get_ordinal(),
+                shuffle=False
+            )
+
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=128, num_workers=4, sampler=test_sampler)
+else:
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=128, shuffle=True, num_workers=36)
+
+    testset = torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=128, shuffle=False, num_workers=36)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
 
-device = 'cpu'
 
 def remove_module_with_prefix(state_dict, prefix='block1'):
     new_state_dict = {}
@@ -90,7 +123,7 @@ if __name__ == '__main__':
     ####################################
     model = Resnet50Truncate()
     original_model_checkpoint_path = './resnet50_logs/version_1/checkpoints/checkpoint-epoch=24-val_acc_epoch=0.8545.ckpt'
-    if device == 'cpu':
+    if device == 'cpu' or device == 'tpu':
         original_checkpoint = torch.load(original_model_checkpoint_path, map_location=lambda storage, loc: storage)
     else:
         original_checkpoint = torch.load(original_model_checkpoint_path)
@@ -123,7 +156,7 @@ if __name__ == '__main__':
     ####################################
     # model = Resnet50Truncate()
     # truncate_model_checkpoint_path = './resnet50_logs/version_1/checkpoints/checkpoint-epoch=24-val_acc_epoch=0.8545.ckpt'
-    # if device == 'cpu':
+    # if device == 'cpu' or device == 'tpu':
     #     truncate_checkpoint = torch.load(truncate_model_checkpoint_path, map_location=lambda storage, loc: storage)
     # else:
     #     truncate_checkpoint = torch.load(truncate_model_checkpoint_path)
