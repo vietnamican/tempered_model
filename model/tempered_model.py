c89d8fd1b09f630e2318a655adefeae38802f7f9
vietnamican <vietnamican@gmail>
2021-02-25 13:14:09 +0700

    fix bug LogitTuneModel

--- model/tempered_model.py
+++ model/tempered_model.py
@@ -283,7 +283,7 @@ class LogitTuneModel(Base):
         self.log('test_loss', loss)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=0.001,
+        optimizer = torch.optim.SGD(self.training_model.parameters(), lr=0.001,
                                     momentum=0.9, weight_decay=5e-4)
         lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
             optimizer, T_max=200)
